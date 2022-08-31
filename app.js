a5ad8441874b6420237d65e50149c0a6fc3554a0
Vittorio <vittoopugliese@gmail.com>
2022-08-31 16:55:19 -0300

    carousel modal nav

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 AOS.init();
+//reload to 0:0
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
+const navToggler = document.querySelector(".navToggler");
+const navContainer = document.querySelector(".navContainer");
+const navContainerBg = document.querySelector(".navContainerBg");
+navToggler.addEventListener("click", () => {
+  navContainer.classList.toggle("navContainerShown");
+  navContainerBg.classList.toggle("navContainerBgShown");
+});
+navContainerBg.addEventListener("click", () => {
+  navContainer.classList.toggle("navContainerShown");
+  navContainerBg.classList.toggle("navContainerBgShown");
+});
