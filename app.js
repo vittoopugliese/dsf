b361e7f66cafc6bf2f3e05baf5dcf2a832ad4a51
Vittorio <vittoopugliese@gmail.com>
2022-09-01 01:15:50 -0300

    all

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ window.onbeforeunload = function () {
 const navToggler = document.querySelector(".navToggler");
 const navContainer = document.querySelector(".navContainer");
 const navContainerBg = document.querySelector(".navContainerBg");
+
 navToggler.addEventListener("click", () => {
   navContainer.classList.toggle("navContainerShown");
   navContainerBg.classList.toggle("navContainerBgShown");
@@ -15,3 +16,14 @@ navContainerBg.addEventListener("click", () => {
   navContainer.classList.toggle("navContainerShown");
   navContainerBg.classList.toggle("navContainerBgShown");
 });
+
+const tok = Array.from(document.querySelectorAll(".tok"));
+
+for (let i = 0; i < tok.length; i++) {
+  tok[i].addEventListener("click", () => {
+    let audio = new Audio();
+    audio.src = "assets/tok.wav";
+    audio.load();
+    audio.play();
+  });
+}
